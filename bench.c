15328f8a846e3ea00f0fd06578676dcb6a51726f
Ilya Kurdyukov <59548320+ilyakurdyukov@users.noreply.github.com>
2021-12-27 16:30:23 +0700

    benchmark update (have unfinished features)

--- bench.c
+++ bench.c
@@ -2,9 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <time.h>
 
+#ifdef TB32_BENCH
+size_t _tb64avx2enc(const unsigned char *in, size_t inlen, unsigned char *out);
+size_t _tb64avx2dec(const unsigned char *in, size_t inlen, unsigned char *out);
+#define crzy64_encode(d, s, n) _tb64avx2enc(s, n, d)
+#define crzy64_decode(d, s, n) _tb64avx2dec(s, n, d)
+#else
 #include "crzy64.h"
+#endif
 
+#ifdef RDTSC_FREQ
+/* useful if you set a static CPU frequency  */
+#include <x86intrin.h>
+#define TIMER_FREQ RDTSC_FREQ
+static int64_t get_time_usec() {
+	unsigned aux;
+	return __rdtscp(&aux);
+}
+#else
+#define TIMER_FREQ 1e6
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
@@ -15,7 +33,6 @@ static int64_t get_time_usec() {
 	return perf.QuadPart * 1000000.0 / freq.QuadPart;
 }
 #else
-#include <time.h>
 #include <sys/time.h>
 static int64_t get_time_usec() {
 	struct timeval time;
@@ -23,17 +40,46 @@ static int64_t get_time_usec() {
 	return time.tv_sec * (int64_t)1000000 + time.tv_usec;
 }
 #endif
+#endif
+
+static uint32_t randseed;
+
+/* not perfect but should be enough */
+static uint32_t bench_rand(uint32_t n) {
+	uint32_t tmp;
+	randseed = randseed * 0xdeece66d + 11;
+	tmp = randseed & ~0u << 16;
+	randseed = randseed * 0xdeece66d + 11;
+	tmp |= randseed >> 16;
+	return tmp * (uint64_t)n >> 32;
+}
 
 int main(int argc, char **argv) {
-	size_t i, n = 100, n1, n2;
+	size_t i, j, n = 100, n1, n2;
 	uint8_t *buf, *out;
-	unsigned nrep = 5;
+	unsigned nrep = 5, nlimit = 100;
 	int64_t t0, t1;
+	float results[3][21];
 
-	if (argc > 1) n = atoi(argv[1]);
-	if (argc > 2) nrep = atoi(argv[2]);
-	if ((nrep - 1) >= 1000) return 1;
-	if (!n || n > 877) return 1; /* >= 2GB */
+	randseed = time(NULL);
+
+	while (argc > 1) {
+		if (argc > 2 && !strcmp(argv[1], "-m")) {
+			n = atoi(argv[2]);
+			if (!n || n > 877) return 1; /* >= 2GB */
+			argc -= 2; argv += 2;
+		} else if (argc > 2 && !strcmp(argv[1], "-r")) {
+			nrep = atoi(argv[2]);
+			if (nrep - 1 >= 1000) return 1;
+			argc -= 2; argv += 2;
+		} else if (argc > 2 && !strcmp(argv[1], "--seed")) {
+			randseed = atoi(argv[2]);
+			argc -= 2; argv += 2;
+		} else if (argc > 2 && !strcmp(argv[1], "--limit")) {
+			nlimit = atol(argv[2]);
+			argc -= 2; argv += 2;
+		} else break;
+	}
 
 	printf("vector: "
 #if CRZY64_VEC && CRZY64_NEON && defined(__aarch64__)
@@ -61,10 +107,12 @@ int main(int argc, char **argv) {
 #else
 		", unaligned: no"
 #endif
-		"\nsize: %u MB\n\n", (int)n);
+		"\nsize: %u MB, repeat: %u, limit: %u\n\n", (int)n, nrep, nlimit);
 
 	n1 = n << 20;
 	n2 = (n1 * 4 + 2) / 3;
+	nlimit = n1 / nlimit;
+	if (nlimit < 1) nlimit = 1;
 
 	if (!(buf = malloc(n1 + n2 + 1))) return 1;
 	out = buf + n1;
@@ -77,31 +125,76 @@ int main(int argc, char **argv) {
 		t0 = get_time_usec() - t0; \
 		if (!i || t0 < t1) t1 = t0; \
 	} \
-	printf(name ": %.3fms (%.2f MB/s)\n", t1 * 0.001, n * 1e6 / t1);
-
-#define BLOCK(fn, d, s, n1, n) do { \
-	size_t j = n1, k; \
-	do j -= k = j > n ? n : j, fn(d, s, k); while (j); \
-} while (0)
-
-#define BENCH_BLOCK(name, n) \
-	BENCH("memcpy" name, BLOCK(memcpy, out, buf, n1, (n * 1000))) \
-	BENCH("encode" name, \
-			BLOCK(crzy64_encode, out, buf, n1, (n * 1000))) \
-	BENCH("decode" name, \
-			BLOCK(crzy64_decode, buf, out, n2, ((n * 1000) * 4 + 2) / 3))
+	BENCH_PRINT(name)
+#define BENCH_PRINT(name) \
+	printf("%s: %.3fms (%.2f MB/s)\n", name, \
+			t1 * (1e6 / TIMER_FREQ * 0.001), n1 * (TIMER_FREQ / (1 << 20)) / t1);
 
 	BENCH("memcpy", memcpy(out, buf, n1))
 	BENCH("encode", crzy64_encode(out, buf, n1))
 	BENCH("decode", crzy64_decode(buf, out, n2))
 	BENCH("encode_unaligned", crzy64_encode(out + 1, buf + 1, n1))
 	BENCH("decode_unaligned", crzy64_decode(buf + 1, out + 1, n2))
-	printf("\nblock repeat:\n");
-	BENCH_BLOCK(" (1Kb)", 1)
-	BENCH_BLOCK(" (10Kb)", 10)
-	BENCH_BLOCK(" (100Kb)", 100)
-	BENCH_BLOCK(" (1Mb)", 1000)
-	BENCH_BLOCK(" (10Mb)", 10000)
-	BENCH_BLOCK(" (20Mb)", 20000)
-#undef BENCH
+
+#undef BENCH_PRINT
+#define BENCH_PRINT(name) \
+	printf("%s (%u%s): %.2f MB/s\n", name, \
+			1 << j % 10, j < 10 ? "" : j < 20 ? "K" : "M", \
+			res = n * (TIMER_FREQ / (1 << 20)) / t1);
+
+#define BENCH_BLOCK \
+	for (j = 0; j <= 20; j++) { \
+		size_t n3 = 1 << j, n4 = (n3 * 4 + 2) / 3; \
+		size_t n5 = n1 / n3, n6 = n2 / n4, nn; \
+		double res; \
+		n6 = n5 < n6 ? n5 : n6; \
+		/* very slow, need limit */ \
+		n5 = n5 < nlimit ? n5 : nlimit; \
+		nn = n = n5 * n3; \
+		BENCH("memcpy", BLOCK(memcpy, out, buf, n3, n3)) \
+		results[0][j] = res; \
+		n5 = n6 < nlimit ? n6 : nlimit; \
+		nn = n = n5 * n3; \
+		BENCH("encode", BLOCK(crzy64_encode, out, buf, n3, n4)) \
+		results[1][j] = res; \
+		nn = n5 * n4; \
+		BENCH("decode", BLOCK(crzy64_decode, buf, out, n4, n3)) \
+		results[2][j] = res; \
+	}
+
+#define BLOCK(fn, d, s, n3, n4) do { \
+	size_t k = nn; \
+	while (k) fn(d, s, n3), k -= n3; \
+} while (0)
+
+	printf("\nblock repeat (cached):\n");
+	BENCH_BLOCK
+
+#undef BLOCK
+#define BLOCK(fn, d, s, n3, n4) do { \
+	size_t k = 0, x = 0; \
+	while (k < nn) fn(d + x, s + k, n3), k += n3, x += n4; \
+} while (0)
+
+	printf("\nblock repeat (consecutive):\n");
+	BENCH_BLOCK
+
+#undef BLOCK
+#define BLOCK(fn, d, s, n3, n4) do { \
+	size_t k = nn, x = n5 * n4; \
+	while (k) k -= n3, x -= n4, fn(d + x, s + k, n3); \
+} while (0)
+
+	printf("\nblock repeat (backwards):\n");
+	BENCH_BLOCK
+
+#undef BLOCK
+#define BLOCK(fn, d, s, n3, n4) do { \
+	size_t k, x; (void)nn; \
+	for (k = 0; k < n5; k++) x = bench_rand(n5), \
+		fn(d + x * n4, s + x * n3, n3); \
+} while (0)
+
+	printf("\nblock repeat (random order):\n");
+	BENCH_BLOCK
 }
